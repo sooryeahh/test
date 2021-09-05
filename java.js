eddf6156ac207874e36a0786680b4e444a9021d7
surya m m <suryachethan358@gmail.com>
2021-09-05 06:31:23 -0700

    Added a list of names

---
+++ java.js
@@ -0,0 +1,6 @@
+console.log('Hey this is my first piece of JS code!!')
+console.log(236524765)
+console.log({name:"Yutika"})
+console.log(false)
+console.log(true)
+console.log(false,215367546,'string',{name:"Alfred"})
